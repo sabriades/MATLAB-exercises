6c9dc04b1666f00cc9f7099cbcf82fe38f87f765
sabriades <sabrinaanna.desiderio@gmail.com>
2024-09-26 13:15:36 +0200

    ex5 and some changes

--- MATLAB.m
+++ MATLAB.m
@@ -1,17 +1,22 @@
 clc
 clear 
 %%
+clc
+clear all 
 
 %Ex1 - inizializzare matrice 4x4 con tutti elementi nulli
-%initialize 4x4 matrix with all null elements
+
 A(4,4)=0; 
+
 %alternatively:
 B=zeros(4,4);
 
 %%
 
 %Ex2 - crea matrice 4x4 con elementi unitari sulla diagonale principale
-%create 4x4 matrix with unit elements on the main diagonal
+clc
+clear all 
+
 C(4,4)=0;
 
 C(1,1)=1;
@@ -32,13 +37,46 @@ E=eye(4,4);
 
 %Ex3 - crea matrice 10x4 popolata con una distribuzione uniforme di numeri
 %interi (casuali) compresi tra 0 e 5
-%eng - creates a 10x4 matrix populated with a uniform distribution of 
-%(random) integers between 0 and 5
+clc
+clear all
 
 F=round(5*rand(10,4));
 
 %alternatively: 
 G=randi([0,5],10,4); 
+
+%%
+
+%Ex4 - Dalla matrice A o B generata nell’esercizio 3, prelevare la sottomatrice 
+% costituita dalle righe dispari e dalle prime tre colonne. Successivamente, 
+% eseguire il plot dei dati assumendo che ogni riga rappresenta un punto nello 
+% spazio euclideo. Assegnare una linea continua rossa e una sfera di colore 
+% rosso a ogni punto.
+
+clc
+clear all
+
+C=randi([0,5],10,4);
+
+l=size(C,1) %estraggo il numero di righe di C nella prima colonna
+k=0; %inizializzo la variabile k
+for i=1:2:l %inizio da 1, passo 2, si finisce quando si arriva a l
+    k=k+1
+    D(k,:)=C(i,:)
+end
+D(:,4)=[] %elimino la 4a colonna
+plot3(D(:,1), D(:,2), D(:,3), 'r-', 'LineWidth', 2, 'Marker', 'o', 'MarkerFaceColor', 'r', 'MarkerSize', 8)
+
+%%
+%Ex5 - dati due punti, calcola la distanza tra essi
+clc
+clear all 
+
+P1=[1,2,1];
+P2=[5,3,3];
+D=P2-P1;
+d=norm(D)
+
 
 
 
