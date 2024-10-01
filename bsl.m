2a97ae1067d709c0046a07ab53bc68b075ff0844
sabriades <sabrinaanna.desiderio@gmail.com>
2024-10-01 10:08:28 +0200

    upload bsl library
    used for MATLAB_ex2

---
+++ bsl.m
@@ -0,0 +1,368 @@
+classdef bsl
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+%&&&                                                                   &&&&
+%&&&                                bsl                                &&&&
+%&&&                                                                   &&&&
+%&&&    routines per il calcolo di B-Spline uniformi non periodiche    &&&&
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+% Last updated: 01 April 2020 
+
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+%
+% Corso di Modellazione Geometrica e Prototipazione Virtuale
+% Prof. Stanislao Patalano
+% a cura di  F. Vitolo - University of Naples
+%            P. Franciosa - University of Warwick
+%
+%&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+    methods (Static)
+        %%
+        function U=knotsNonPeriodic(n,p)
+            % calcola il vettore dei nodi (uniforme-non periodico)
+            % INPUT:
+            %      n - numero di punti di controllo-1
+            %      p - grado della curva ... se p=n si ha una curva di Bezier
+            
+            U=[]; % inizializza U
+            if p>n
+                msgbox('P-degree should be less then %d', n)
+                return
+            end
+            
+            k=p+1; % ordine della curva
+            m=n+k;
+            U=zeros(1,m+1);
+            
+            for i=0:m
+                if i<k % prima condizione
+                    U(i+1)=0;
+                elseif i>=k && i<=n % seconda condizione
+                    U(i+1)=i-k+1;
+                elseif i>n % terza condizione
+                    U(i+1)=n-k+2;
+                end
+            end
+            
+            U=U./max(U);
+        end
+        
+        %%
+        function spanv=getSpan(U)
+            % calcola gli span-knots
+            spanv=[];
+            for i=1:length(U)-1
+                if U(i+1)>U(i)
+                    spanv=[spanv;U(i) U(i+1)];
+                end
+            end
+        end
+        
+        %%
+        function spank=findSpanKnot(u,n,U)
+            % calcola lo span-knot index
+            % u - parametro della curva (u deve appartenere allo span-knot [ui,ui+1[.. estremo destro aperto
+            % n - numero di punti di controllo-1
+            % U - vettore dei nodi
+            
+            % spank - span-knot al quale "u" appartiene (0-based')
+            % Attenzione - 0-based: il primo intervallo è definito con indice 0. 
+            
+            % inizializza...
+            spank=[];
+            
+            % caso particolare se u==U(n+2)...==U($-p-1)
+            if u==U(n+2)
+                spank=n;
+                return
+            end
+            
+            Uspan=[U(1:end-1);U(2:end)];
+            for i=1:size(Uspan,2)
+                if u>=Uspan(1,i) && u<Uspan(2,i)
+                    spank=i;
+                    spank=spank-1;
+                    break
+                end
+            end
+        end
+        
+        %%
+        function N=basicFunctionBspline(i,u,p,n,U)
+            % calcola le funzioni di forma non nulle
+            % i - span index
+            % u - parametro del punto
+            % p - grado della curva
+            % n - numero di punti di controllo-1
+            % U - vettore dei nodi
+            % N - vettore delle funzioni di forma [1,n+1]
+            
+            % i=i-1; // zero-based
+            
+            tN(1)=1;
+            
+            for j=1:p
+                left(j+1)=u-U((i+1-j)+1);
+                rigth(j+1)=U((i+j)+1)-u;
+                
+                saved=0;
+                for r=0:j-1
+                    temp=tN(r+1)/(rigth((r+1)+1)+left((j-r)+1));
+                    tN(r+1)=saved+rigth((r+1)+1)*temp;
+                    saved=left((j-r)+1)*temp;
+                end
+                tN(j+1)=saved;
+            end
+            
+            N=zeros(1,n+1);
+            
+            N(i-p+1:i+1)=tN';
+        end
+        
+        %%
+        function Pbs=getBsplinePoint(Pc,p,U,ulow,uup,res)
+            % calcola i punti della B-spline
+            % Pc - coordinate dei punti di controllo [n+1,2]
+            % U - vettore dei nodi ([1xn+k+1])... k=p+1
+            % uup/ulow - intervallo compreso tra [0, 1] nel quale plottare la curva
+            % p - grado della curva
+            % res - risoluzione della curva
+            
+            % Pbs - coordinate dei punti della curva B-spline
+            
+            Pbs=[];
+            
+            % # di punti di controllo-1
+            n=size(Pc,1)-1;
+            
+            % dimensione dello spazio di lavoro
+            nspace=size(Pc,2);
+            
+            % ordine della curva
+            k=p+1;
+            
+            % vettore parametrico
+            u=ulow:(uup-ulow)/(res-1):uup;
+            
+            % numero di punti complessivi
+            nPoint=length(u);
+            
+            % inizializza l'output
+            Pbs=zeros(nPoint,nspace);
+            
+            % loop over all points
+            for point=1:nPoint
+                
+                % calcola l'indice di appartenenza
+                spank=bsl.findSpanKnot(u(point),n,U);
+                
+                % calcola le funzioni di base
+                N=bsl.basicFunctionBspline(spank,u(point),p,n,U);
+                
+                % aggiorna i punti della curva
+                Pbs(point,:)=N*Pc;
+            end
+        end
+        
+        %%
+        function [Pc,U]=globalCurveInterp(Q,p)
+            % interpolazione globale con curva B-spline
+            % Q - vettore dei punti da interpolare
+            % p - grado della curva B-spline
+            % Pc - vettore dei punti di controllo
+            % U - vettore dei nodi (uniforme non periodico)
+            
+            n=size(Q,1)-1;
+            
+            nc=size(Q,2);
+            Pc=zeros(n+1,nc);
+            
+            % calcola il vettore dei parametri ("chord length alghoritm")
+            dt=0;
+            for i=1:n
+                d(i)=norm(Q(i+1,:)-Q(i,:));
+                dt=d(i)+dt;
+            end
+            
+            % normalizzato nell'intervallo [0, 1]
+            t(1)=0;
+            for i=2:n
+                t(i)=t(i-1)+d(i-1)/dt;
+            end
+            
+            t(end+1)=1;
+            
+            % calcola il knot vector ("average alghoritm")
+            m=n+p+1;
+            U=zeros(1,m+1);
+            
+            U(1:p+1)=0;
+            U(m-p+1:end)=1;
+            
+            for j=1:n-p
+                U(j+p+1)=sum(t(j+1:j+p))/p;
+            end
+            
+            % inizializza la matrice dei coefficienti
+            A=zeros(n+1,n+1);
+            
+            % fill A rows
+            for i=1:n+1
+                
+                id=bsl.findSpanKnot(t(i),n,U);
+                N=bsl.basicFunctionBspline(id,t(i),p,n,U);
+                
+                A(i,:)=N;
+            end
+            
+            % calcola i punti di controllo di controllo incogniti
+            Pc=A\Q; % LU decomposition
+        end
+        
+        %%
+        function writePointonFile(filename,Pbs)
+            % esporta i punti della curva
+            % scrivi i punti della curva su file
+            fileID=fopen(filename,'w');
+            fprintf(fileID,'%.3f %.3f %.3f\n',Pbs');
+            fclose(fileID);
+        end
+        
+        
+        %%
+        %&&&&&&&&&&&&&&&&&&
+        % GRAPHIC UTILITIES
+        %&&&&&&&&&&&&&&&&&&
+        
+        %%
+        function createCurve(Pc,p,U,res)
+            % Pc - punti di controllo
+            % p - grado della curva
+            % U - vettore dei nodi
+            
+            ax=gca();
+            
+            Uk=bsl.getSpan(U);
+            
+            % setta le opzioni
+            opt.thickness=2;
+            opt.color='blue';
+            opt.style='-';
+            
+            ncurve=size(Uk,1);
+            for i=1:ncurve
+                Pbs=bsl.getBsplinePoint(Pc,p,U,Uk(i,1),Uk(i,2),res);
+                
+                bsl.drawCurve(Pbs,opt,ax, Pc)
+                
+                if strcmp(opt.style,'-')
+                    opt.style='--';
+                elseif strcmp(opt.style,'--')
+                    opt.style='-';
+                end
+                
+            end
+            
+            view(2);
+        end
+        
+        %%
+        function drawCurve(Point,opt,ax, Pc)
+            % Point - coordinate dei nodi
+            % opt - struttura delle opzioni
+            % ax - asse corrente
+            % Pc - on/off poligono di controllo
+            
+            if size(Point,2)==2 % se curva 2D aggiungi al terna coordinata
+                Point(:,3)=0;
+            end
+            
+            hold all
+            
+            plot3(Point(:,1),Point(:,2),Point(:,3))
+            
+            % % assegna le proprietà
+            obj=get(ax, 'Children'); % get current entity
+            hobj=obj(1);
+            hobj.LineWidth=opt.thickness;
+            hobj.LineStyle=opt.style;
+            hobj.Color=opt.color;
+            
+            % plotta il poligono di controllo
+            if ~isempty(Pc)
+                
+                if size(Pc,2)==2 % se curva 2D aggiungi al terna coordinata
+                    Pc(:,3)=0;
+                end
+                
+                plot3(Pc(:,1),Pc(:,2),Pc(:,3),'k')
+                
+                % assegna le proprietà
+                obj=get(ax, 'Children'); % get current entity
+                hobj=(obj(1));
+                hobj.LineStyle='-';
+                hobj.LineWidth=1;
+                hobj.Color='k';
+                hobj.Marker='square';
+                
+            end
+            
+            % setta le opzioni dell'asse
+            axis equal
+            set(gca,'XGrid','on','YGrid','on')
+            hold all
+        end
+        
+        %%
+        function N=drawN(n,p,U,res)
+            % plotta le funzioni di base
+            % n - numero di punti di controllo - 1
+            % p - grado della curva
+            % U - vettore dei nodi
+            % res - risoluzione grafica
+            
+            % vettore parametrico
+            t=0:1/(res-1):1;
+            
+            for z=1:length(t)
+                i=bsl.findSpanKnot(t(z),n,U);
+                N(z,:)=bsl.basicFunctionBspline(i,t(z),p,n,U);
+            end
+            
+            plot(t',N)
+            grid()
+            
+            xlabel('t curve [0, 1]')
+            ylabel('Basic function, N')
+        end
+        
+        %%
+        function plotCloudPoint(Q,msize)
+            % plotta nuvola di punti
+            % msize - dimensione grafica del punto
+            
+            nc=size(Q,2);
+            
+            if nc==3
+                plot3(Q(:,1),Q(:,2),Q(:,3))
+                
+                obj=get(gca,'Children'); % get current entity
+                h=(obj(1));
+                h.LineStyle='none';
+                h.Marker='square';
+                h.MarkerSize=msize;
+                h.MarkerFaceColor='k';
+            else
+                plot(Q(:,1),Q(:,2),'ks')
+                obj=get(gca,'Children'); % get current entity
+                h=(obj(1));
+                h.MarkerSize=msize;
+                h.MarkerFaceColor='k';
+            end
+        end
+        
+    end
+end
+        
+        
