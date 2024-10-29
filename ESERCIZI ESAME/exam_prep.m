439c67021f7c182762be930a743d61cc06f9830f
sabriades <sabrinaanna.desiderio@gmail.com>
2024-10-29 23:19:04 +0100

    wip

--- ESERCIZI ESAME/exam_prep.m
+++ ESERCIZI ESAME/exam_prep.m
@@ -44,3 +44,87 @@ figure("Name","curva bspline","NumberTitle","off");
 bsl.createCurve(Pc,p,U,res);
 title("bspline");
 
+
+%% esercizio 2 
+
+clc
+clear all 
+
+%dati 7 Pc con p=4 della curva, calcola e visualizza le funzioni base della
+%bspline. identifica l'intervallo del vettore dei nodi che contiene t=0.3 e
+%calcola i valori delle funzioni di base per questo parametro
+
+Pc = [1 2 2 
+      3 4 5 
+      5 3 6 
+      7 8 4
+      9 10 8
+      4 3 7 
+      6 5 3]; 
+p=4; 
+t=0.3; %in quale intervallo del vettore dei nodi è contenuto questo valore? 
+u=t;
+
+n=size(Pc,1)-1;
+U=bsl.knotsNonPeriodic(n,p); %vettore dei nodi
+Uk=bsl.getSpan(U); %intervallo del vettore dei nodi
+Uk %tratti del vettore dei nodi (intervalli per ogni tratto)
+i=bsl.findSpanKnot(u,n,U);
+disp("t=0.3 è contenuto nell'intervallo:");
+disp([U(i+1),U(i+2)]);
+N=bsl.basicFunctionBspline(i,u,p,n,U); %funzioni di base per t=u=0.3
+
+%plot funzioni di base
+res=100;
+figure("Name","funzioni di base","NumberTitle","off");
+N=bsl.drawN(n,p,U,res);
+title("funzioni di base per t=0.3");
+
+%plot bspline
+figure("Name","curva bspline","NumberTitle","off");
+bsl.createCurve(Pc,p,U,res);
+title("curva bspline");
+
+
+%% esercizio 3
+
+clc
+clear all 
+
+%dati 6 Pc in uno spazio 3d, definire il vettore dei nodi della bspline con
+%p=2. determina il valore del parametro t=0.7, identifica l'intervallo del
+%vettore dei nodi a cui appartiene e calcola i valori delle funzioni di
+%miscelamento per questo valore di t. traccia la curva risultante 
+
+Pc=[0 1 0 
+    2 3 5 
+    4 6 1 
+    3 4 3 
+    5 2 6 
+    7 3 2]; 
+
+%vettore dei nodi
+p=2; 
+n=size(Pc,1)-1; 
+U=bsl.knotsNonPeriodic(n,p); %calcola il vettore dei nodi
+
+%t=0.7
+t=0.7;
+u=t;
+Uk=bsl.getSpan(U); %intervalli del vettore dei nodi
+Uk %stampa Uk a video
+i=bsl.findSpanKnot(u,n,U);
+disp("t appartiene all'intervallo");
+disp([U(i+1),U(i+2)]);
+
+%calcolo le funzioni di miscelamento
+N=bsl.basicFunctionBspline(i,u,p,n,U);
+res=100; 
+figure("Name","funzioni di miscelamento","NumberTitle","off");
+N=bsl.drawN(n,p,U,res);
+title("funzioni di miscelamento per t=0.7");
+
+%curva bspline
+figure("Name","curva bspline","NumberTitle","off");
+bsl.createCurve(Pc,p,U,res);
+title("bspline");
